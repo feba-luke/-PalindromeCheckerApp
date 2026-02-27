a8f80491663df0002d3e7548c8285fed17edc852
feba-luke <febaluke02@gmail.com>
2026-02-27 14:44:33 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,10 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome checker Management System");
+        System.out.println("Version:1.0");
+        System.out.println("System Initialized Succesfully");
+    }
 }
+
+
+
