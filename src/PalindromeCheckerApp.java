352e2ae48e6b1b08ca24701f8d74efe0d60774a7
feba-luke <febaluke02@gmail.com>
2026-02-27 15:35:35 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,30 +3,8 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to Palindrome checker Management System");
         System.out.println("Version:1.0");
         System.out.println("System Initialized Succesfully");
-        String input = "madam";
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("String: \"" + input + "\"");
-        if (isPalindrome) {
-            System.out.println("Result: The string is a palindrome.");
-        } else {
-            System.out.println("Result: The string is NOT a palindrome.");
-        }
-    }
-    private  static boolean check(String s, int start, int end) {
 
-        if (start >= end) {
-            return true;
-        }
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
 
-        return check(s, start + 1, end - 1);
     }
+
 }
